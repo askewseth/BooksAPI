ff8f82589e8476d1abef21961ab3f12556d3e094
Seth Askew <saskew@cree.com>
2018-04-08 18:10:40 -0400

    Changed the build script to only run the continaer if the 'run' flag is given, otherwise it just builds the container and tags it for docker hub.

--- build.sh
+++ build.sh
@@ -6,5 +6,9 @@ env GOOS=linux GOARCH=amd64 go build -o books-api main.go
 #build the container
 docker build -t askewseth/books-api .
 
-#run the container
-docker run -p 5555:5555 --name books-api -d askewseth/books-api
+
+#run the container if the run flag is given
+if [[ "$1" == "run" ]]; then
+	docker run -p 5555:5555 --name books-api -d askewseth/books-api
+fi
+
