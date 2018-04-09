92d745c983b61b6c511bf8daf32792347c8938e5
seth <askewseth@gmail.com>
2018-04-08 20:50:32 -0400

    Added ability to get status of pod in k8 script.

--- k8.sh
+++ k8.sh
@@ -1,32 +1,37 @@
 #!/bin/bash
 
-
 help() {
 	echo "Available commands are:"
-	echo " - expose: expose the ports of the pod after it is created"
 	echo " - create: create the pod"
+	echo " - expose: expose the ports of the pod after it is created"
+	echo " - status: print the status of the pod"
 	echo " - delete: delete the pod"
 }
 	
 create() {
 	kubectl create -f pod-books-api.yml
-	exit 0
+	exit $?
 }
 
 expose() {
 	kubectl port-forward books-api 5555
-	exit 0
+	exit $?
 }
 
 delete() {
 	kubectl delete pod books-api
-	exit 0
+	exit $?
+}
+
+status() {
+	kubectl get pods books-api | awk '{print $3}' | tail -n1
+	exit $?
 }
 
 case "$1" in
 	expose) expose ;;
 	create) create ;;
 	delete) delete ;;
+	status) status ;;
+	*)      help ;;
 esac
-
-help
